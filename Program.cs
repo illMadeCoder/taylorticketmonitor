07043dc93fb9bad731e405f0559e75b9faa00e21
nannyjasmine <jazzisutton@gmail.com>
2023-04-20 00:10:55 -0400

    m

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@ using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add this inside the `ConfigureServices` method
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// Add this inside the `ConfigureServices` method
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(app.Configuration.GetConnectionString("DefaultConnection")));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
