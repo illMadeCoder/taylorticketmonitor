2b3bd0a9f32cdd3c7b18f415aa1f38bfe5fb21c7
illmadecoder <illmadecoder@gmail.com>
2023-04-22 18:48:27 -0400

    section

--- Event.cs
+++ Event.cs
@@ -1,6 +1,7 @@
 public class Event
 {
     public int id { get; set; }
+    public string? section { get; set; }
     public int price { get; set; }
     public string? img { get; set; }
     public string? url { get; set; }
